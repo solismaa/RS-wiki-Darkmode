30bf48c7b3a8b6f70431234c62198925292abb11
CephHunter <pieterpartous@hotmail.com>
2018-04-14 00:16:00 +0200

    Userpage tags

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('less', function() {
 gulp.task('serve', ['less'], function() {
     browserSync.init({
         proxy: {
-            target: "http://cephhunter.wikia.com",
+            target: "http://runescape.wikia.com",
             cookies: {stripDomain: false}
         },
         plugins: ['bs-rewrite-rules'],
