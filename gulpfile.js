9381c0218373e2a681192865639872397e1aa948
CephHunter <pieterpartous@hotmail.com>
2018-06-03 02:15:22 +0200

    Updated to version 2.0.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+'use strict';
 var gulp = require('gulp');
 var less = require('gulp-less');
 var plumber = require('gulp-plumber');
@@ -5,25 +6,28 @@ var browserSync = require('browser-sync').create();
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cleanCSS = require('gulp-clean-css');
-var sessionID = [0];
-try {
-    sessionID = require("./sessionID.json");
-} catch (error) {
-    console.log("No sessionID found.");
-}
+var safeImportant = require('postcss-safe-important');
+var rename = require("gulp-rename");
+var sourcemaps = require('gulp-sourcemaps');
+var bump = require('gulp-bump');
+var header = require('gulp-header');
+var fs = require('fs');
+var runSequence = require('run-sequence');
 
-gulp.task('less', function() {
+// For development
+gulp.task('dev', function() {
     return gulp.src('./.less/darkmode.less')
         .pipe(plumber())
+        .pipe(sourcemaps.init())
         .pipe(less())
-        .pipe(postcss([ autoprefixer() ]))
+        .pipe(postcss([ autoprefixer(), safeImportant()]))
+        .pipe(rename('Darkmode.dev.css'))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./'))
-        .pipe(cleanCSS({level: 1}))
-        .pipe(gulp.dest('minified'))
         .pipe(browserSync.stream());
-});
+})
 
-gulp.task('serve', ['less'], function() {
+gulp.task('default', ['dev'], function() {
     browserSync.init({
         proxy: {
             target: "http://runescape.wikia.com",
@@ -34,24 +38,74 @@ gulp.task('serve', ['less'], function() {
         rewriteRules: [
             {
                 match: "</body>",
-                replace: '<link rel="stylesheet" type="text/css" href="/Darkmode.css"></body>'
+                replace: '<link rel="stylesheet" type="text/css" href="/Darkmode.dev.css"></body>'
             },
             {
                 match: "<head>",
-                replace: "<head>" + Cookies()
+                replace: "<head><script>document.cookie = 'euCookiePolicy=1'</script>"
             }
         ]
     });
 
     gulp.watch(".less/*.less", ['less']);
+});   
+
+// Generate the final cleaned up css and minified files
+gulp.task('clean', function() {
+    var obj = JSON.parse(fs.readFileSync('./package.json'));
+    var d = new Date();
+    var months = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
+    var headerMsg = 
+    "/**\n" +
+    " * =================================\n" +
+    " *     Runescape wiki darkmode\n" +
+    " * =================================\n" +
+    " * Version: " + obj.version + "\n" +
+    " * Release date: " + d.getDate() + "-" + months[d.getMonth()] + "-" + d.getFullYear() + "\n" +
+    " * Website: https://github.com/CephHunter/RS-wiki-DarkMode\n" +
+    " * License: CC-BY-SA-4.0\n" +
+    " * \n" +
+    " * If on stylish for firefox, add '@-moz-document domain('runescape.wikia.com') { }' around code\n" +
+    " */\n";
+
+    return gulp.src('.less/darkmode.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(postcss([ autoprefixer(), safeImportant()]))
+        .pipe(cleanCSS({level: {2: {all: true}}, format: 'beautify'}))
+        .pipe(header(headerMsg))
+        .pipe(gulp.dest('./'))
+        .pipe(rename('Darkmode.min.css'))
+        .pipe(cleanCSS({level: 0}))
+        .pipe(gulp.dest('./'));
+
 });
 
-gulp.task('default', ['serve']);
+// Bump version number
+gulp.task('_patch', function() {
+    return gulp.src('package.json')
+        .pipe(bump({type: 'patch'}))
+        .pipe(gulp.dest('./'));
+})
 
-function Cookies() {
-    var res = '<script>'
-            + 'document.cookie = "access_token=' + sessionID[0] + '";'
-            + 'document.cookie = "euCookiePolicy=1"'
-            + '</script>';
-    return res;
-}
+gulp.task('_minor', function() {
+    return gulp.src('package.json')
+        .pipe(bump({type: 'minor'}))
+        .pipe(gulp.dest('./'));
+})
+
+gulp.task('_major', function() {
+    return gulp.src('package.json')
+        .pipe(bump({type: 'major'}))
+        .pipe(gulp.dest('./'));
+})
+
+gulp.task('patch', function(callback) {
+    runSequence('_patch', 'clean', callback);
+})
+gulp.task('minor', function(callback) {
+    runSequence('_minor', 'clean', callback);
+})
+gulp.task('major', function(callback) {
+    runSequence('_major', 'clean', callback);
+})
